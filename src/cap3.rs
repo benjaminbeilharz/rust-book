ad4fbd01af6ae6c7a77444f7d15584221b1c76f8
Benjamin Beilharz <b@beilharz.io>
2021-12-18 17:40:02 +0100

    cap3 added

--- src/cap3.rs
+++ src/cap3.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 pub fn solve() {
-   unimplemented!("This still needs to be implemented"); 
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let b = {
+        let c = 4;
+        c + 1
+    };
+
+    println!("The value of b is {}", b);
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {} is {}", index, element);
+    
 }
